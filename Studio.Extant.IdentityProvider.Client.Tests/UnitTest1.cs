aeeff663cdc3d9ff1950d9b1e264b96083f2c22e
Jeremy McKee <jsmckee@me.com>
2025-01-02 16:07:28 -0600

    Update

---
+++ Studio.Extant.IdentityProvider.Client.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Studio.Extant.IdentityProvider.Client.Tests
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
+    {
+
+    }
+  }
+}
---
+++ Studio.Extant.IdentityProvider.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Studio.Extant.IdentityProvider.Tests
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
+    {
+
+    }
+  }
+}
